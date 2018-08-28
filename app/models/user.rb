2d531e5e801e9a39fafc5691fdbfd6535bcd1fc4
Stephen Malek <malek.stephen44@gmail.com>
2018-08-28 10:46:10 -0600

    validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :username, :email, :password_digest
+  validates_uniqueness_of :email, :auth_token, :activation_token
   has_secure_password
   enum status: [:inactive, :active]
 end
