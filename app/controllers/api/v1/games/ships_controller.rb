8d6b901c5db8d301f1f3840e228c349c27069581
Stephen Malek <malek.stephen44@gmail.com>
2018-08-28 20:53:56 -0600

    try to get spec harness to work

---
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::Games::ShipsController < ApiController
+  def create
+    
+  end
+end
--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -8,4 +8,8 @@ class Api::V1::GamesController < ActionController::API
       render status: :bad_request
     end
   end
+
+  def create
+    render json: Game.create(player_1_board: Board.new(4), player_2_board: Board.new(4))
+  end
 end
