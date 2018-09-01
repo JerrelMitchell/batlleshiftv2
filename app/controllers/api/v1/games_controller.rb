34b542058602fa93fbca81785507682abb40d205
JerrelMitchell <jerrelmitchell@ymail.com>
2018-09-01 14:57:45 -0600

    change inheritance to apicontroller and remove redundant user1 method to be current_user, memoized find_game and user2

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::GamesController < ActionController::API
+class Api::V1::GamesController < ApiController
   def show
     if find_game
       render json: find_game
@@ -9,25 +9,22 @@ class Api::V1::GamesController < ActionController::API
 
   def create
     new_game = set_game_boards
-    new_game.user_games.create(user_id: user_2.id, player_type: 1)
+    new_game.user_games.create(user_id: user2.id, player_type: 1)
     render json: new_game
   end
 
   private
 
   def set_game_boards
-    user_1.games.create(player_1_board: Board.new, player_2_board: Board.new)
+    current_user.games.create(player_1_board: Board.new,
+                              player_2_board: Board.new)
   end
 
   def find_game
-    Game.find_by(id: params[:id])
+    @find_game ||= Game.find_by(id: params[:id])
   end
 
-  def user_1
-    User.find_by(auth_token: request.headers.env['HTTP_X_API_KEY'])
-  end
-
-  def user_2
-    User.find_by(email: params[:opponent_email])
+  def user2
+    @user2 ||= User.find_by(email: params[:opponent_email])
   end
 end
