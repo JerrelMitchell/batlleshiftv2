cdef8025211760e20ef0d429f49b01a72a248179
Stephen Malek <malek.stephen44@gmail.com>
2018-09-01 14:04:10 -0600

    make commented out test pass, refactor ships controller to use place ships presenter WIP

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::Games::ShipsController < ApiController
   def create
-    game = Game.find(params[:game_id])
-    user = game.users.find_by(auth_token: request.headers.env['HTTP_X_API_KEY'])
-    if user.user_games.first.player_type == "challenger"
+    presenter = PlaceShipPresenter.new(params[:game_id], current_user).authenticate
+
+    game = current_user.games.find(params[:game_id]) if current_user
+
+    if current_user.user_games.first.player_type == "challenger"
       board = game.player_1_board
       ship_placer = ShipPlacer.new(board: board, ship: board.get_ship, start_space: params[:start_space], end_space: params[:end_space])
       ship_placer.ship.place(params[:start_space], params[:end_space])
@@ -10,7 +12,7 @@ class Api::V1::Games::ShipsController < ApiController
       game.update(current_turn: 1) if game.player_1_board.ships_to_place.empty?
       game.update(player_1_board: ship_placer.board)
       render json: game, message: MessageGenerator.new.place_ship(params[:ship_size], board.ships_to_place)
-    elsif user.user_games.first.player_type == "opponent"
+    elsif current_user.user_games.first.player_type == "opponent"
       board = game.player_2_board
       ship_placer = ShipPlacer.new(board: board, ship: board.get_ship, start_space: params[:start_space], end_space: params[:end_space])
       ship_placer.ship.place(params[:start_space], params[:end_space])
@@ -18,5 +20,17 @@ class Api::V1::Games::ShipsController < ApiController
       game.update(current_turn: 0) if game.player_1_board.ships_to_place.empty?
       render json: game, message: MessageGenerator.new.place_ship(params[:ship_size], board.ships_to_place)
     end
+  end
+end
+
+class PlaceShipPresenter
+
+  def initialize(game_id, current_user)
+    @game = current_user.games.find(game_id) if current_user
+    @user = current_user
+  end
+
+  def authenticate
+    return self unless @game.nil?
   end
 end
